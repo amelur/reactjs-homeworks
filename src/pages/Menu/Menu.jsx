df6a702f7eb2c8780578b8d62e9b47c937974fc3
amelur <ameluckin@gmail.com>
2025-11-08 14:47:54 +0200

    feat: add solution for HW4

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -8,28 +8,42 @@ const Menu = ({ onAddToCart }) => {
     const [meals, setMeals] = useState([]);
     const [visibleMeals, setVisibleMeals] = useState([]);
     const [startIndex, setStartIndex] = useState(0);
+    const [selectedCategory, setSelectedCategory] = useState("Dessert")
     const ITEMS_PER_PAGE = 6;
 
-    const isAllShown = visibleMeals.length >= meals.length;
-
+    const filterMealsByCategory = (category, data = meals) => {
+        return data.filter((meal) => meal.category === category);
+    };
 
     useEffect(() => {
         fetch("https://65de35f3dccfcd562f5691bb.mockapi.io/api/v1/meals")
             .then(res => res.json())
             .then(data => {
                 setMeals(data);
-                setVisibleMeals(data.slice(0, ITEMS_PER_PAGE));
+                const filtered = filterMealsByCategory(selectedCategory, data);
+                setVisibleMeals(filtered.slice(0, ITEMS_PER_PAGE));
                 setStartIndex(ITEMS_PER_PAGE);
             })
             .catch(err => console.error("Ошибка загрузки меню:", err));
     }, []);
 
+    useEffect(() => {
+        const filtered = filterMealsByCategory(selectedCategory);
+        setVisibleMeals(filtered.slice(0, ITEMS_PER_PAGE));
+        setStartIndex(ITEMS_PER_PAGE);
+    }, [selectedCategory, meals]);
+
     const handleSeeMore = () => {
+        const filtered = filterMealsByCategory(selectedCategory);
         const nextIndex = startIndex + ITEMS_PER_PAGE;
-        const nextMeals = meals.slice(startIndex, nextIndex);
-        setVisibleMeals(prev => [...prev, ...nextMeals]);
+        const nextMeals = filtered.slice(startIndex, nextIndex);
+        setVisibleMeals((prev) => [...prev, ...nextMeals]);
         setStartIndex(nextIndex);
     };
+
+    const filtered = filterMealsByCategory(selectedCategory);
+    const isAllShown = visibleMeals.length >= filtered.length;
+
 
     return (
         <main>
@@ -42,9 +56,15 @@ const Menu = ({ onAddToCart }) => {
                             place a pickup order. Fast and fresh food.</p>
                     </div>
                     <div className={styles['btns-wrapper']}>
-                        <Button text="Dessert" size="medium"/>
-                        <Button text="Dinner" size="medium" secondary/>
-                        <Button text="Breakfast" size="medium" secondary/>
+                        {["Dessert", "Dinner", "Breakfast"].map((category) => (
+                            <Button
+                                key={category}
+                                text={category}
+                                size="medium"
+                                onClick={() => setSelectedCategory(category)}
+                                secondary={selectedCategory !== category}
+                            />
+                        ))}
                     </div>
                     <MenuList meals={visibleMeals} onAddToCart={onAddToCart}/>
                     {!isAllShown && (
