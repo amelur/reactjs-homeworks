f0830a97dcde5c2bff691d524d91d53c7cb39972
amelur <ameluckin@gmail.com>
2025-11-30 16:15:48 +0200

    feat: complete hw task 6

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import styles from "./Header.module.scss";
 import NavBar from "../NavBar/NavBar";
 import CartBtn from "../CartBtn/CartBtn";
+import {Link} from "react-router-dom";
 
 export default function Header({totalMeals}) {
     return (
         <header>
             <div className={`wrapper ${styles.header__wrapper}`}>
-                <a href="/" className='logo'></a>
+                <Link to="/" className='logo'></Link>
                 <div className={styles.container}>
                     <NavBar/>
                     <CartBtn totalMeals={totalMeals}/>
